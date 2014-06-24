575d7acf62406057da4e4552b0501dbce55862de
Graeme Wilkinson <graemewilkinson@Graeme-Wilkinsons-MacBook-Pro.local>
2014-06-24 11:44:06 +0100

    udpated mysqli definition

--- phpsample/connect-db.php
+++ phpsample/connect-db.php
@@ -1,11 +1,12 @@
 <?php
-// Connect to the database
-$hostname = "mysql.gwilki01.wdd1314.bbkweb.org";
-$username = "gwilki01";
-$password = "Edahze45imei";
-$database = "gwilki01";
- 
- // Connect to Database
-$link = mysql_connect($hostname,$username,$password);
-mysql_select_db($database) or die("Unable to select database");
+
+// server info
+$server = 'mysql.gwilki01.wdd1314.bbkweb.org';
+$user = 'gwilki01';
+$pass = 'Edahze45imei';
+$db = 'gwilki01';
+
+// connect to the database
+$mysqli = new mysqli($server, $user, $pass, $db);
+
 ?>
