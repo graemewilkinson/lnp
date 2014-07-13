edb8834a2548557b6cbb364aaabd99567d28a168
graemewilkinson <graeme.wilkinson@me.com>
2014-07-13 19:41:12 +0100

    contact form

---
+++ newsite/contact.php
@@ -0,0 +1,49 @@
+<html>
+<body>
+<?php
+function spamcheck($field) {
+  // Sanitize e-mail address
+  $field=filter_var($field, FILTER_SANITIZE_EMAIL);
+  // Validate e-mail address
+  if(filter_var($field, FILTER_VALIDATE_EMAIL)) {
+    return TRUE;
+  } else {
+    return FALSE;
+  }
+}
+?>
+
+<h2>Feedback Form</h2>
+<?php
+// display form if user has not clicked submit
+if (!isset($_POST["submit"])) {
+  ?>
+  <form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
+  From: <input type="text" name="from"><br>
+  Subject: <input type="text" name="subject"><br>
+  Message: <textarea rows="10" cols="40" name="message"></textarea><br>
+  <input type="submit" name="submit" value="Submit Feedback">
+  </form>
+  <?php 
+} else {  // the user has submitted the form
+  // Check if the "from" input field is filled out
+  if (isset($_POST["from"])) {
+    // Check if "from" email address is valid
+    $mailcheck = spamcheck($_POST["from"]);
+    if ($mailcheck==FALSE) {
+      echo "Invalid input";
+    } else {
+      $from = $_POST["from"]; // sender
+      $subject = $_POST["subject"];
+      $message = $_POST["message"];
+      // message lines should not exceed 70 characters (PHP rule), so wrap it
+      $message = wordwrap($message, 70);
+      // send mail
+      mail("graeme.wilkinson@me.com",$subject,$message,"From: $from\n");
+      echo "Thank you for sending us feedback";
+    }
+  }
+}
+?>
+</body>
+</html>
