b6cb3dbbfbb40625094eaf8ada1598a41e54b727
graemewilkinson <graeme.wilkinson@me.com>
2014-07-23 17:04:53 +0100

    working on php to create mosiac page

---
+++ newsite/testimg.php
@@ -0,0 +1,25 @@
+<?php
+// Connect to the database
+// Normal kept in a separate file and used with include()
+$hostname = "mysql.gwilki01.wdd1314.bbkweb.org";
+$username = "gwilki01";
+$password = "Edahze45imei";
+$database = "gwilki01";
+
+$link = mysql_connect($hostname,$username,$password);
+mysql_select_db($database) or die("Unable to select database");
+
+$sql ='SELECT id, companyname, mainimg, mainimgdes FROM testimonials ORDER BY id DESC';
+
+// run the query
+$result = mysql_query($sql,$link) or die("Unable to select: ".mysql_error());
+
+print "<div class='clearfix mosaicflow'>\n\n";
+while ($row = mysql_fetch_row($result)) {
+    list($id, $companyname, $mainimg, $imgdes) = $row;
+    print "<div class='mosaicflow__item'>\n";
+    print "<a href=\'testpage.php?id=$id'\">\n<img src='img/$mainimg' alt='$mainimgdes'>\n<p>$companyname</p></a>\n";
+    print "</div>\n";
+}
+print "</div>\n";
+?>
