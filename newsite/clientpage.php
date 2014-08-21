16ec3af85e70931b80f6f1acd2fe21c532e52c60
graemewilkinson <graeme.wilkinson@me.com>
2014-08-21 14:06:27 +0100

    seperating query

--- newsite/clientpage.php
+++ newsite/clientpage.php
@@ -106,19 +106,24 @@ $database = "gwilki01";
 $link = mysql_connect($hostname,$username,$password);
 mysql_select_db($database) or die("Unable to select database");
 
-$sql ="SELECT testimonials.id, testimonials.companyname, testimonials.brief, img.imagelink, img.imagedes FROM testimonials INNER JOIN img ON testimonials.id=img.id WHERE testimonials.id = " . $_GET['id'];
+$sql ="SELECT imagelink, imagedes FROM img WHERE id = " . $_GET['id'];
+$sql ="SELECT companyname FROM testimonials WHERE id = " . $_GET['id'];
 
 // run the query
 $result = mysql_query($sql,$link) or die("Unable to select: ".mysql_error());
 
-print "<div class='clearfix mosaicflow'>\n";
+
 while ($row = mysql_fetch_row($result)) {
-    list($id, $companyname, $imagelink, $imagedes) = $row;
+    
+    list($imagelink, $imagedes) = $row;
+    print "<h1>$companyname</h1>\n";
+    print "<div class='clearfix mosaicflow'>\n";
     print "<div class='mosaicflow__item'>\n";
     print "<img src='img/$imagelink' alt='$imagedes'>";
     print "</div>\n";
 }
 print "</div>\n";
+
 ?>                                  
 <div style="clear: both;"></div>
 </div>
