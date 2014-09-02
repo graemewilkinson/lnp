6feab4be8d23c0397050e99d90b7d782e98a6300
graemewilkinson <graeme.wilkinson@me.com>
2014-09-02 23:07:57 +0100

    removing password

--- phpsample/connect-db.php
+++ phpsample/connect-db.php
@@ -1,1 +1,12 @@
+<?php
 
+// server info
+$server = 'mysql.gwilki01.wdd1314.bbkweb.org';
+$user = 'gwilki01';
+$pass = 'moneky';
+$db = 'gwilki01';
+
+// connect to the database
+$mysqli = new mysqli($server, $user, $pass, $db);
+
+?>
