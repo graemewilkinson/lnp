077900fd3f746b7567065af7dfcb5a783f086929
graemewilkinson <graeme.wilkinson@me.com>
2014-09-05 19:25:41 +0100

    add variables back in

--- newsite/index.php
+++ newsite/index.php
@@ -10,10 +10,12 @@ include 'header.php';
 <?php
 include 'login.php';
 
+$link = mysql_connect($hostname,$username,$password);
+mysql_select_db($database) or die("Unable to select database");
+
 $sql = 'SELECT sliderlink, des FROM slider ORDER BY pos';
 
 // run the query
-
 $result = mysql_query($sql,$link) or die("Unable to select: ".mysql_error());
 
 while ($row = mysql_fetch_row($result)) {
@@ -30,11 +32,6 @@ while ($row = mysql_fetch_row($result)) {
       </p>
             <div class='testblock'>
 <?php
-include 'login.php';
-
-$link = mysql_connect($hostname,$username,$password);
-mysql_select_db($database) or die("Unable to select database");
-
 $sql = 'SELECT testimonials.id, testimonials.companyname, testimonials.testimonialcontent, testimonials.name, img.imagelink, img.imagedes, testimonials.ref, img.logo FROM testimonials LEFT JOIN img ON testimonials.id=img.id AND img.logo = TRUE ORDER BY testimonials.id DESC LIMIT 3';
 
 // run the query
